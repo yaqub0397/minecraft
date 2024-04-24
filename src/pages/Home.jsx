35bf3e1f3317fc0cca5b7ecc17ad977aa092ef31
MD YAQUB ANSARI <79414606+yaqub0397@users.noreply.github.com>
2024-04-24 18:59:41 +0530

    Updates

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Navbar, Main, Product, Footer } from "../components";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Product />
+    </>
+  )
+}
+
+export default Home
